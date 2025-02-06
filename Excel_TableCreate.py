067baecfd0436c50b749933d428f5d2e5574b319
67Fujiwara <yuutafujiwara67@yahoo.co.jp>
2025-02-07 02:05:02 +0900

    更新
    subwindow1のGUI作成とgetｺﾏﾝﾄﾞ作成中

--- Excel_TableCreate.py
+++ Excel_TableCreate.py
@@ -12,14 +12,16 @@ import tkinter as tk
 import pyperclip
 import sys
 import time
+import tkinter.font as f
+
 
 
 class WindowController:
-    def __init__(self, master, title, bg_color):
+    def __init__(self, master, title, bg_color, win_size):
         self.master = master
         self.master.title(title)
         self.master.config(bg=bg_color)
-        self.master.geometry("360x150")
+        self.master.geometry(win_size)
         
        
     def create_button(self, text, command, bg_color, font_color, xpos, ypos, width, height):
@@ -27,11 +29,29 @@ class WindowController:
         button.place(x=xpos, y=ypos, width=width, height=height)
         
         
-    def create_label(self, text, bg_color, font_color, xpos, ypos, width, height, label_title):
+    def create_labelframe(self, text, bg_color, font_color, xpos, ypos, width, height, label_title):
         self.labelframe = tk.LabelFrame(self.master, text=label_title, bg=bg_color, fg=font_color)
         self.labelframe.place(x=xpos-10, y=ypos-20, width=width+30, height=height+30)
         label = tk.Label(self.master, text=text, bg=bg_color, fg=font_color)
         label.place(x=xpos, y=ypos, width=width, height=height)
+ 
+    
+    def create_label(self, justify_pos, text, row, column, width):
+        font1 = f.Font(family="Lucida Console", weight="bold", size=12, slant="italic")
+        label = tk.Label(self.master, justify=justify_pos, text=text, bg="white")
+        label["font"] = font1
+        label.grid(row=row, column=column)
+        entry = tk.Entry(self.master, width=width, justify=justify_pos)
+        entry.grid(row=row , column=column + 1)
+        return entry
+    
+    
+    def create_entry(self, width, row, column):
+        entry2 = tk.Entry(self.master, width=width)
+        entry2.grid(row=row , column=column)
+        return entry2
+    
+
  
 class MainApp:
     def __init__(self, root):
@@ -40,11 +60,13 @@ class MainApp:
         self.root = root  
         self.bg_white = "white"
         self.font_black = "black"
-        win_control = WindowController(root, "create_table", self.bg_white)
+        win_control = WindowController(root, "create_table", self.bg_white, "360x150")
         check_button = win_control.create_button("check", self.open_win1, self.bg_white, self.font_black, 40, 20, 100, 50)
         value_button = win_control.create_button("value", self.open_win2, self.bg_white, self.font_black, 210, 20, 100, 50)
-        win_control.create_label(self.file_path, self.bg_white, self.font_black, 20, 100, 300, 20, "File_Path")
+        win_control.create_labelframe(self.file_path, self.bg_white, self.font_black, 20, 100, 300, 20, "File_Path")
         # self.sub_win1.create_combobox(sub_win1, combo_list)
+       
+        # self.get_data()
      
         self.check_table()
         self.create_table()
@@ -60,50 +82,62 @@ class MainApp:
             print("ファイルパスが指定されていません。")
             
    
+    
+   
     def input_data(self):
         self.file_path = Path(r"C:\Users\yuta_\OneDrive\デスクトップ\aaa.xlsx")
         self.wb = load_workbook(self.file_path, data_only=True)
         self.ws_list = self.wb.sheetnames
         self.ws = self.wb.worksheets[1]
-        
+             
         
     def check_table(self):
-        self.ref_column = "K"    #基準横軸
-        self.ref_row =40     #基準縦軸
+        self.ref_column = self.getAlphabet_cell    #基準横軸
+        self.ref_row = self.getNumber_cell     #基準縦軸
         self.deadline_row = self.ref_row
         self.task_row = self.ref_row + 1
         self.employee_column = self.ref_column
         
         self.reference_cell = [self.ref_column + str(self.ref_row)]
-        self.input_columns = 3   #横軸   
-        self.input_rows = 3       #縦軸
+        # self.reference_cell = self.ref_cell
+        self.input_columns = self.getNumber_column   #横軸   
+        self.input_rows = self.getNumber_row       #縦軸
         self.cell_employee = 1   #社員ｺｰﾄﾞ用ｾﾙ
         self.cell_task = 2  #期限用ｾﾙ + ﾀｽｸ用ｾﾙ  
         return self.ref_column
      
+     
     def open_win1(self):
-        self.check_sub = self.create_sub1("sub1", self.bg_white, "300x300")
+        self.check_sub = self.create_sub1()
 
     def open_win2(self):
-        self.value_sub = self.create_sub2("sub2", self.bg_white, "300x300")
+        self.value_sub = self.create_sub2()
 
 
-    def create_sub1(self, title, bg_color, win_size):
+    def create_sub1(self):
         sub_win1 = Toplevel(self.root)
-        sub_win1.title(title)
-        sub_win1.config(bg=bg_color)
-        sub_win1.geometry(win_size)
-        
-        
-    def create_sub2(self, title, bg_color, win_size):
+        self.sub1_control = WindowController(sub_win1, "check_table", self.bg_white, "250x200")
+        self.sub1_control.create_label("center", "シート", 0, 0, 10)
+        self.alphabet_cell = self.sub1_control.create_label("center", "基準セル", 1, 0, 5)
+        self.number_cell = self.sub1_control.create_entry(5, 1, 2 )
+        self.number_column = self.sub1_control.create_label("center", "列数", 2, 0, 10)
+        self.number_row = self.sub1_control.create_label("center", "行数", 3, 0, 10)
+        self.sub1_control.create_button("書き込み", lambda:self.get_data(), self.bg_white, self.font_black, 100, 100, 50, 30)
+   
+   
+    def create_sub2(self):
         sub_win2 = Toplevel(self.root)
-        sub_win2.title(title)
-        sub_win2.config(bg=bg_color)
-        sub_win2.geometry(win_size)
-            
-
+        self.sub2_control = WindowController(sub_win2, "value_table", self.bg_white, "300x200")
+    
+    
+    def get_data(self):
+       self.getAlphabet_cell = self.alphabet_cell.get()
+       self.getNumber_cell = int(self.number_cell.get())
+       self.getNumber_column = int(self.number_column.get())
+       self.getNumber_row = int(self.number_row.get())
+   
+       
     def create_table(self):
-
         line = openpyxl.styles.Side(style="thin", color="000000")     #普通線・黒色
         border = openpyxl.styles.Border(top=line, bottom=line, left=line, right=line)      #上下左右を線に適応
         self.ws.column_dimensions[self.ref_column].width = 14
