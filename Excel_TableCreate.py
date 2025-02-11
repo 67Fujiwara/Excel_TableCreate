7c8881fe1e2e5606df32de6f1b889ad83cdebe11
67Fujiwara <yuutafujiwara67@yahoo.co.jp>
2025-02-11 22:20:42 +0900

    Zの次のAAにも描画できるようにした

--- Excel_TableCreate.py
+++ Excel_TableCreate.py
@@ -1,4 +1,4 @@
-import openpyxl
+import openpyxl as xl
 from openpyxl.styles.borders import Border, Side
 from openpyxl.styles import PatternFill
 from pathlib import Path
@@ -13,10 +13,10 @@ import pyperclip
 import sys
 import time
 import tkinter.font as f
-
+from openpyxl.worksheet.datavalidation import DataValidation
 import sys
-print(sys.executable)
 
+# print(sys.executable)
 
 
 class WindowController:
@@ -50,11 +50,18 @@ class WindowController:
     
     
     def create_label2(self, justify_pos, text, row, column):
-        font1 = f.Font(family="Lucida Console", weight="bold", size=12, slant="italic")
+        font = f.Font(family="Lucida Console", weight="bold", size=12, slant="italic")
         label = tk.Label(self.master, justify=justify_pos, text=text, bg="white")
-        label["font"] = font1
+        label["font"] = font
         label.grid(row=row, column=column)
     
+    
+    def create_label3(self, justify_pos, text, xpos, ypos, width, height):
+        font = f.Font(family="Lucida Console", weight="bold", size=12, slant="italic")
+        self.label = tk.Label(self.master, justify=justify_pos, text=text, bg="white", fg="green")
+        self.label["font"] = font
+        self.label.place(x=xpos, y=ypos, width=width, height=height)
+        
     
     def create_entry(self, width, row, column):
         entry2 = tk.Entry(self.master, width=width)
@@ -79,8 +86,6 @@ class WindowController:
 
 class MainApp:
     def __init__(self, root):
-        # self.get_path()
-        # self.input_flag = False
         self.input_data() 
         self.root = root  
         self.bg_white = "white"
@@ -89,43 +94,34 @@ class MainApp:
         check_button = win_control.create_button("check", self.open_win1, self.bg_white, self.font_black, 40, 20, 100, 50)
         value_button = win_control.create_button("value", self.open_win2, self.bg_white, self.font_black, 210, 20, 100, 50)
         win_control.create_labelframe(file_path, self.bg_white, self.font_black, 20, 100, 300, 20, "File_Path")
-        # self.get_data()
    
-        
-    # def get_path(self):
-    #     if len(sys.argv) > 1:
-    #         self.file_path = sys.argv[1]
-    #         pyperclip.copy(self.file_path)
-    #         print(f"ファイルパスがクリップボードにコピーされました: {self.file_path}")  
-    #     else:
-    #         print("ファイルパスが指定されていません。")
-    #     return self.file_path
-    
-   
+
     def input_data(self):
-        # self.file_path = Path(r"C:\Users\yuta_\OneDrive\デスクトップ\aaa.xlsx")
         self.wb = load_workbook(file_path, data_only=True)
         self.ws_list = self.wb.sheetnames
-       
+    
+    
+    def column_to_number(self, column):
+        number = 0
+        for c in column.upper():
+            number = number * 26 + (ord(c) - ord('A') + 1)
+        return number
+    
         
     def input_check_table(self):
-        print("input_check_table_on")
         self.ws = self.wb[self.sub1_control.ws_result]
         self.wb.active = self.ws
-        # self.ws = self.wb.active
         self.ref_column = self.getAlphabet_cell    #基準横軸
         self.ref_row = self.getNumber_cell     #基準縦軸
         self.deadline_row = self.ref_row
         self.task_row = self.ref_row + 1
-        self.employee_column = self.ref_column
         
         self.reference_cell = [self.ref_column + str(self.ref_row)]
-        # self.reference_cell = self.ref_cell
         self.input_columns = self.getNumber_column   #横軸   
         self.input_rows = self.getNumber_row       #縦軸
         self.cell_employee = 1   #社員ｺｰﾄﾞ用ｾﾙ
         self.cell_task = 2  #期限用ｾﾙ + ﾀｽｸ用ｾﾙ  
-        
+        self.column_number = self.column_to_number(self.ref_column)
         self.create_table()
         return self.ref_column
      
@@ -147,68 +143,64 @@ class MainApp:
         self.number_column = self.sub1_control.create_label("center", "列数", 2, 0, 10)
         self.number_row = self.sub1_control.create_label("center", "行数", 3, 0, 10)
         self.sub1_control.create_button("書き込み", lambda:self.on_button_click(), self.bg_white, self.font_black, 100, 100, 50, 30)
-   
-   
+        # self.sub1_control.create_button("Excel開く", lambda:self.open_excel(), self.bg_white, self.font_black, 150, 100, 50, 30)
+        
     def create_sub2(self):
         sub_win2 = Toplevel(self.root)
         self.sub2_control = WindowController(sub_win2, "value_table", self.bg_white, "300x200")
     
     
     def on_button_click(self):
-       self.getAlphabet_cell = self.alphabet_cell.get()
-       self.getNumber_cell = int(self.number_cell.get())
-       self.getNumber_column = int(self.number_column.get())
-       self.getNumber_row = int(self.number_row.get())
-       self.input_flag = True     #ﾎﾞﾀﾝonのﾌﾗｸﾞ
-       print(self.sub1_control.ws_result)
-       self.input_check_table()
+        self.getAlphabet_cell = self.alphabet_cell.get()
+        self.getNumber_cell = int(self.number_cell.get())
+        self.getNumber_column = int(self.number_column.get())
+        self.getNumber_row = int(self.number_row.get())
+        self.input_check_table()
        
+
     def create_table(self):
-        print("create_table_on")
         line = openpyxl.styles.Side(style="thin", color="000000")     #普通線・黒色
         border = openpyxl.styles.Border(top=line, bottom=line, left=line, right=line)      #上下左右を線に適応
         self.ws.column_dimensions[self.ref_column].width = 14
-        
-        number = ord(self.ref_column.upper()) - ord('A') + 1
         for j in range(0, self.input_rows + 2):    #縦軸に枠線を描画  input_rows + deadline + taskline
-            count_row = self.ref_row + j
-            for i in string.ascii_uppercase[number-1 : (number-1) + self.input_columns + self.cell_employee]:     #横軸に枠線を描画
-                print(i + str(count_row))
-                cell = self.ws[i + str(count_row)]
+            row = self.ref_row + j
+            for index in range(0, self.input_columns + self.cell_employee): #横軸に枠線を描画  
+                # print(self.column_number + index, row, index)
+                cell = self.ws.cell(row, self.column_number + index)
                 cell.border = border
                 cell.alignment = Alignment(horizontal = 'center',
                                            vertical = 'center',
                                            wrap_text = False)
-                
-                if self.employee_column == i:
-                    # employee_line
-                    employee_color = openpyxl.styles.PatternFill(fgColor="ffe3c0", bgColor="ffe3c0", fill_type="solid")
-                    cell.fill = employee_color    
-                else:
-                    pass
-                
-                if self.deadline_row == count_row:
+                    
+                if self.deadline_row == row:
                     # deadline_line
                     deadline_color = openpyxl.styles.PatternFill(fgColor="a7f542", bgColor="a7f542", fill_type="solid")
                     cell.fill = deadline_color
-                    if self.ref_column == i:
+                    if self.column_number == self.column_number + index:
                         cell.value = "deadline"
-                else:
-                    pass
-            
-                if self.task_row == count_row:
+
+                elif self.task_row == row:
                     # task_line
                     task_color = openpyxl.styles.PatternFill(fgColor="fed5f8", bgColor="fed5f8", fill_type="solid")
                     cell.fill = task_color
-                    if self.ref_column == i:
+                    if self.column_number == self.column_number + index:
                         cell.value = "taskline"
+                        
+                elif self.column_number == self.column_number + index:
+                    # employee_line
+                    employee_color = openpyxl.styles.PatternFill(fgColor="ffe3c0", bgColor="ffe3c0", fill_type="solid")
+                    cell.fill = employee_color           
+                        
                 else:
+                    # char_to_column = chr(63 + self.column_number + index)
+                    # dv = DataValidation(type="list", formula1='"〇,△,×"', showDropDown=True)
+                    # self.ws.add_data_validation(dv)
+                    # dv.add(self.ws[str(row),char_to_column])
+                    # print(str(row),char_to_column)
                     pass
     
         self.wb.save(file_path)
-        print('save')
-    
-
+        self.save_comp = self.sub1_control.create_label3("center", "Excel書き込み完了", 25, 150, 200, 30)
 
 if len(sys.argv) > 1:
     file_path = sys.argv[1]
